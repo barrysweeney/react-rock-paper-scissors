d1e21655f0f842b0f42258136f0feab58eb056a9
barrysweeney <barrysweeney172@live.co.uk>
2020-04-04 16:04:30 +0100

    Updates

---
+++ precache-manifest.ff29d86537fcac9005af6928f5db44e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f03c78ad1149ec0bed51a2cf5438fb",
+    "url": "/react-rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "e1ed2d20460b8544ddc3",
+    "url": "/react-rock-paper-scissors/static/css/main.4e38ec4f.chunk.css"
+  },
+  {
+    "revision": "1704b7a38b927988ee76",
+    "url": "/react-rock-paper-scissors/static/js/2.218f7357.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-rock-paper-scissors/static/js/2.218f7357.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1ed2d20460b8544ddc3",
+    "url": "/react-rock-paper-scissors/static/js/main.4448a84d.chunk.js"
+  },
+  {
+    "revision": "1e768a93c4689e940401",
+    "url": "/react-rock-paper-scissors/static/js/runtime-main.3caa5a5d.js"
+  }
+]);
